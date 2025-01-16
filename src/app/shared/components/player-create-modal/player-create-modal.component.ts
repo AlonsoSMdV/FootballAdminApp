e5ac05b7825ed5b591a51c443cf9442384e9d2cc
AlonsoSMdV <alonsosmdv@gmail.com>
2025-01-16 14:24:25 +0100

    Added date picker instied write the birthdate

--- src/app/shared/components/player-create-modal/player-create-modal.component.ts
+++ src/app/shared/components/player-create-modal/player-create-modal.component.ts
@@ -1,7 +1,8 @@
-import { Component, Input, OnInit } from '@angular/core';
-import { FormGroup, FormBuilder, Validators } from '@angular/forms';
+import { DatePipe } from '@angular/common';import { Component, Input, OnInit } from '@angular/core';
+import { FormGroup, FormBuilder, Validators, FormControl } from '@angular/forms';
 import { ModalController, Platform } from '@ionic/angular';
 import { Player } from 'src/app/core/models/players.model';
+
 
 @Component({
   selector: 'app-player-create-modal',
@@ -9,9 +10,12 @@ import { Player } from 'src/app/core/models/players.model';
   styleUrls: ['./player-create-modal.component.scss'],
 })
 export class PlayerCreateModalComponent  implements OnInit {
+  getBirthdate: FormControl;
   formGroup:FormGroup;
   mode:'new'|'edit' = 'new';
   isMobile: boolean = false;
+  isDatePickerOpen = false;
+
 
 
   @Input() set player(_player:Player){
@@ -31,8 +35,10 @@ export class PlayerCreateModalComponent  implements OnInit {
   constructor(
     private fb:FormBuilder,
     private modalCtrl:ModalController,
-    private platform: Platform
+    private platform: Platform,
+    
   ) { 
+    this.getBirthdate = new FormControl('');
     this.isMobile = this.platform.is('ios') || this.platform.is('android');
     this.formGroup = this.fb.group({
       name:['', [Validators.required, Validators.minLength(2)]],
@@ -46,6 +52,19 @@ export class PlayerCreateModalComponent  implements OnInit {
     });
   }
   
+  openDatePicker() {
+    this.isDatePickerOpen = true;
+  }
+
+  onDateChange(event: any) {
+    const rawDate = new Date(event.detail.value); // Convierte el valor recibido a un objeto Date
+
+  // Si prefieres 'dd-MM-yyyy'
+  const formattedDate = `${rawDate.getDate().toString().padStart(2, '0')}-${(rawDate.getMonth() + 1).toString().padStart(2, '0')}-${rawDate.getFullYear()}`;
+
+  this.birthdate.setValue(formattedDate); // Asigna el valor formateado al control de formulario
+  this.isDatePickerOpen = false; // Cierra el modal del date picker
+  }
 
   ngOnInit() {}
 
