908385bda18b2538f94cb03cb388af099f48d47b
Alonso <asanmor011004@gmail.com>
2025-04-08 19:36:43 +0200

    New page implemented needs to implement update roles

--- src/app/pages/users/users.page.ts
+++ src/app/pages/users/users.page.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { Users } from 'src/app/core/models/users.model';
+import { UsersService } from 'src/app/core/services/impl/users.service';
 
 @Component({
   selector: 'app-users',
@@ -6,10 +8,34 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./users.page.scss'],
 })
 export class UsersPage implements OnInit {
+  users: Users[] = [];
 
-  constructor() { }
+  constructor(private usersService: UsersService) {}
 
   ngOnInit() {
+    this.loadUsers();
   }
 
+  loadUsers(event?: any): void {
+    this.usersService.getAll().subscribe({
+      next: (data: any) => {
+        if (Array.isArray(data)) {
+          this.users = data;
+        } else {
+          this.users = data?.data || [];
+        }
+        if (event) event.target.complete();
+      },
+      error: err => {
+        console.error('Error cargando usuarios:', err);
+        if (event) event.target.complete();
+      }
+    });
+  }
+
+  updateUserRole(user: any) {
+    // Aquí iría tu llamada a API o lógica
+    console.log(`Rol actualizado a "${user.role}" para:`, user);
+    // this.userService.updateRole(user.id, user.role).subscribe(...)
+  }
 }
