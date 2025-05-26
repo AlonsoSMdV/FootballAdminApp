701ec431f7ebe10d418044ec6acc30069fbae15d
Alonso <asanmor011004@gmail.com>
2025-05-26 22:31:48 +0200

    small changes, and implemented user favs, next steps improve pages styles

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -5,6 +5,11 @@ import { BaseAuthenticationService } from './core/services/impl/base-authenticat
 import { LanguageService } from './core/services/language.service';
 import { BehaviorSubject, lastValueFrom, Observable } from 'rxjs';
 import { UsersService } from './core/services/impl/users.service';
+import { LeagueService } from './core/services/impl/league.service';
+import { Team } from './core/models/teams.model';
+import { TeamService } from './core/services/impl/team.service';
+import { PlayerService } from './core/services/impl/player.service';
+import { Users } from './core/models/users.model';
 
 @Component({
   selector: 'app-root',
@@ -23,7 +28,10 @@ export class AppComponent implements OnInit{
     public authSvc: BaseAuthenticationService,
     private router: Router,
     private activatedRoute: ActivatedRoute,
-    public userSvc: UsersService
+    public userSvc: UsersService,
+    public leagueSvc: LeagueService,
+    public teamSvc: TeamService,
+    public playerSvc: PlayerService
   ) {
     this.currentLang = this.languageService.getStoredLanguage();
   }
@@ -60,6 +68,42 @@ export class AppComponent implements OnInit{
       filter(([isAuthenticated, user]) => isAuthenticated && !!user),
       map(([isAuthenticated, user]) => ({ isAuthenticated, user }))
     );
+    // Suscribirse para cargar nombres favoritos
+    this.userWithAuth$.subscribe(({ user }) => {
+      this.loadFavoriteNames(user);
+    });
+  }
+
+  favoriteNames = {
+    league: '',
+    leagueImg: '',
+    team: '',
+    teamImg: '',
+    player: '',
+    playerImg: ''
+  };
+  
+  loadFavoriteNames(user: Users) {
+    if (user.leagueFav) {
+      this.leagueSvc.getById(user.leagueFav).subscribe(league => {
+        this.favoriteNames.league = league?.name || '';
+        this.favoriteNames.leagueImg = league?.picture?.url || ''; // o el campo real de imagen
+      });
+    }
+  
+    if (user.teamFav) {
+      this.teamSvc.getById(user.teamFav).subscribe(team => {
+        this.favoriteNames.team = team?.name || '';
+        this.favoriteNames.teamImg = team?.picture?.url || '';
+      });
+    }
+  
+    if (user.playerFav) {
+      this.playerSvc.getById(user.playerFav).subscribe(player => {
+        this.favoriteNames.player = `${player?.name} ${player?.firstSurname}` || '';
+        this.favoriteNames.playerImg = player?.picture?.url || '';
+      });
+    }
   }
 
   /*isDarkTheme = false;
