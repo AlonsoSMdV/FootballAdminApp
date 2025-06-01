c292433990cb6f35760fe1ad89709fdb661ab949
Alonso <asanmor011004@gmail.com>
2025-06-01 19:07:15 +0200

    simple changes

--- tailwind.config.js
+++ tailwind.config.js
@@ -7,7 +7,17 @@ module.exports = {
     "./src/**/*.scss"
   ],
   theme: {
-    extend: {},
+    extend: {
+      keyframes: {
+        shine: {
+          '0%': { backgroundPosition: '200% center' },
+          '100%': { backgroundPosition: '-200% center' },
+        },
+      },
+      animation: {
+        shine: 'shine 2s linear infinite',
+      },
+    },
   },
   plugins: [],
 }
